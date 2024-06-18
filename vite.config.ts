d606f03d69b8a8526e3598a33fdd49736f4d24a8
Jesus Ruiz <ruizramos04@outlook.com>
2024-06-18 14:23:41 -0400

    init setup complete

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://churuizramos.github.io/myPortfolio-vue',
   plugins: [
     vue(),
     vueJsx(),
