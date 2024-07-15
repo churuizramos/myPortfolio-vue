4d2fb33fc0faabbfc1845a672bd641e476d06563
Jesus <ruizramos04@outlook.com>
2024-07-15 01:15:48 -0400

    WIP header backup

---
+++ src/components/CustomTestHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex space-x-4 p-16 bg-emerald-950 content-center">
+    <img src="/square_thumbnail_IMG_9503.jpg" alt="Headshot of myself" class="size-1/4 inline-block rounded-full"/>
+    <!--  header text  -->
+    <div class="inline-block">
+      <h1 class="text-4xl text-white">Jesus Ruiz Ramos</h1>
+      <h2 class="text-4 text-white font-bold">Computer Science and Engineering</h2>
+      <a href="https://louisville.edu"><h1 class="text-white text-3">University of Louisville <span class="text-red-500">|</span> J.B. Speed School of Engineering</h1></a>
+      <a href="https://github.com/churuizramos/" target="_blank" class="inline-block"><img src="/icons8-github-64.png" alt="GitHub icon" class="size-8"/></a>
+      <a href="https://www.linkedin.com/in/jesus-ruiz-ramos-9591a5238/" target="_blank" class="inline-block"><img src="/icons8-linkedin-64.png" alt="LinkedIn icon" class="size-8"/></a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
