1a08935ded23dd794d453d6f18a4df9b8a273fcf
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-07-15 16:26:18 -0400

    refactored header component and removed initialized vue components

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template class="p-16 space-y-4 bg-emerald-950">
+  <div class="p-16 space-y-4 bg-emerald-950">
+    <h1 class="text-4xl text-white">Jesus (Chuco) Ruiz Ramos</h1>
+    <h2 class="text-4 text-white font-bold">Computer Science and Engineering</h2>
+    <div>
+      <a href="http://louisville.edu"><h1 class="text-3 text-white">University of Louisville <span class="text-red-500">|</span> J.B Speed School of Engineering</h1></a>
+      <a href="https://github.com/churuizramos/" target="_blank" class="inline-block"><img src="/icons8-github-64.png" alt="GitHub icon" class="size-8"/></a>
+      <a href="https://www.linkedin.com/in/jesus-ruiz-ramos-9591a5238/" target="_blank" class="inline-block"><img src="/icons8-linkedin-64.png" alt="LinkedIn icon" class="size-8"/></a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
