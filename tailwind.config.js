93ef9609facf589880f0b46933226fcfd0618ffd
Jesus <ruizramos04@outlook.com>
2024-07-15 23:32:28 -0400

    added references to custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,30 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
+    darkMode: false,
     extend: {
       backgroundColor: {
-        'beige': '#F5F5DC',
-      }
+        'c-emerald-1': '#80be9f',
+        'c-emerald-2': '#669675',
+        'c-emerald-3': '#2a4849',
+        'c-emerald-4': '#203524',
+
+        'c-orange-1': '#d79130',
+        'c-orange-2': '#7e3a14',
+      },
+      textColor: {
+        'c-emerald-1': '#80be9f',
+        'c-emerald-2': '#669675',
+        'c-emerald-3': '#2a4849',
+        'c-emerald-4': '#203524',
+
+        'c-orange-1': '#d79130',
+        'c-orange-2': '#7e3a14',
+      },
+      fontFamily: {
+        'archivo-black': ["Archivo Black"],
+        'archivo-narrow': ["Archivo Narrow"],
+      },
     },
   },
   plugins: [],
