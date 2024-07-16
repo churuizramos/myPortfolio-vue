ca5643bbae196d0cc03fd4e58aac62d2cbdcb8e6
Jesus <ruizramos04@outlook.com>
2024-07-15 23:31:38 -0400

    header setup, almost done

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -2,14 +2,21 @@
 
 </script>
 
-<template class="p-16 space-y-4 bg-emerald-950">
-  <div class="p-16 space-y-4 bg-emerald-950">
-    <h1 class="text-4xl text-white">Jesus (Chuco) Ruiz Ramos</h1>
-    <h2 class="text-4 text-white font-bold">Computer Science and Engineering</h2>
+<template>>
+  <div class="space-y-4 bg-c-emerald-3 px-10 py-16 text-c-orange-1 font-archivo-narrow">
     <div>
-      <a href="http://louisville.edu"><h1 class="text-3 text-white">University of Louisville <span class="text-red-500">|</span> J.B Speed School of Engineering</h1></a>
-      <a href="https://github.com/churuizramos/" target="_blank" class="inline-block"><img src="/icons8-github-64.png" alt="GitHub icon" class="size-8"/></a>
-      <a href="https://www.linkedin.com/in/jesus-ruiz-ramos-9591a5238/" target="_blank" class="inline-block"><img src="/icons8-linkedin-64.png" alt="LinkedIn icon" class="size-8"/></a>
+      <h1 class="text-2xl">&commat;churuizramos</h1>
+      <h1 class="text-8xl text-gray-300 font-archivo-black">JESUS RUIZ RAMOS</h1>
+    </div>
+    <div>
+      <a href="http://louisville.edu" target="_blank"><h1 class="text-3 hover:bg-gray-300 hover:text-c-orange-2 px-2 transition transition-duration-300 rounded-full">University of Louisville <span class="text-red-500">|</span> J.B Speed School of Engineering</h1></a>
+      <h2 class="text-5xl text-gray-300 font-bold">Computer Science and Engineering</h2>
+      <div class="text-2xl">
+        <a href="https://github.com/churuizramos/" target="_blank" class="inline-block hover:bg-gray-300 transition transition-duration-300 rounded-full"><img src="/icons8-github.svg" alt="GitHub icon" class="size-10 align-middle"/></a>
+        <a href="https://www.linkedin.com/in/jesus-ruiz-ramos-9591a5238/" target="_blank" class="inline-block hover:bg-gray-300 transition transition-duration-300 rounded-full"><img src="/icons8-linkedin.svg" alt="LinkedIn icon" class="size-10 align-middle"/></a>
+        <a href="mailto:j0ruiz08@louisville.edu" class="inline-block text-c-orange-1 hover:text-c-orange-2 hover:bg-gray-300 transition transition-duration-300 align-top my-1 mx-0.5 px-2 rounded-2xl"><h2>school email</h2></a>
+        <a href="mailto:ruizramos04@outlook.com" class="inline-block text-c-orange-1 hover:text-c-orange-2 hover:bg-gray-300 transition transition-duration-300 align-top my-1 mx-0.5 px-2 rounded-2xl"><h2>personal email</h2></a>
+      </div>
     </div>
   </div>
 </template>
