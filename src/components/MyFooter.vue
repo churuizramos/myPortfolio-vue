89d52f0f94d9ff088990d4c310011eb2ac7ae3f6
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-08-19 10:12:26 -0400

    quick restructure for the images. Placed them witin the p tags

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -6,23 +6,31 @@
   <div class="footer-custom">
     <div class="footer-aside-custom"> <!-- left side -->
       <a href="https://instagram.com/churuizramos" target="_blank" class="footer-link">
-        <img src="/icons8-instagram.svg" alt="instagram icon" class="footer-img-c"/>
-        <p class="footer-p-c">&commat;churuizramos</p>
+        <p class="footer-p-c">
+          <img src="/icons8-instagram.svg" alt="instagram icon" class="footer-img-c"/>
+          &commat;churuizramos
+        </p>
       </a>
       <a href="https://" target="_blank" class="footer-link">
-        <img src="/icons8-twitter.svg" alt="twitter icon" class="footer-img-c"/>
-        <p class="footer-p-c">&commat;churuizramos</p>
+        <p class="footer-p-c">
+          <img src="/icons8-twitter.svg" alt="twitter icon" class="footer-img-c"/>
+          &commat;churuizramos
+        </p>
       </a>
     </div>
 
     <div class="footer-aside-custom"> <!-- right side -->
       <a href="mailto:ruizramos04@outlook.com" class="footer-link">
-        <img src="/icons8-mail.svg" alt="mail icon" class="footer-img-c"/>
-        <p class="footer-p-c">ruizramos04&commat;outlook&period;com</p>
+        <p class="footer-p-c">
+          <img src="/icons8-mail.svg" alt="mail icon" class="footer-img-c"/>
+          ruizramos04&commat;outlook&period;com
+        </p>
       </a>
       <a href="mailto:j0ruiz08@louisville.edu" class="footer-link">
-        <img src="/icons8-mail.svg" alt="mail icon" class="footer-img-c"/>
-        <p class="footer-p-c">j0ruiz08&commat;louisville&period;com</p>
+        <p class="footer-p-c">
+          <img src="/icons8-mail.svg" alt="mail icon" class="footer-img-c"/>
+          j0ruiz08&commat;louisville&period;com
+        </p>
       </a>
     </div>
   </div>
